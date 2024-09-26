099058a209569cffe3cba76fa2c2850227870fc3
ssura <thsqls123@naver.com>
2024-09-26 15:12:41 +0900

    240926

--- 240926/main.tf
+++ 240926/main.tf
@@ -19,3 +19,30 @@ resource "aws_vpc" "stu6_example" {
     Name = "stu6-simple-vpc"
   }
 }
+
+# Create a Instance
+resource "aws_instance" "stu6-simple" {
+  ami                    = "ami-0a70c5266db4a6202"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  tags = {
+    Name = "stu6-simple-server"
+  }
+  # 프로비저닝 시, 유저데이터를 사용하도록 설정
+  #EOF 앞에 - 기호 사용 시, tab 사용이 가능
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, Terraform!!" > index.html
+              nohup busybox httpd -f -p "${var.web_port}" &
+              EOF
+}
+
+resource "aws_security_group" "instance" {
+  name = "stu6-simple-weg-sg"
+  ingress {
+    from_port   = var.web_port
+    to_port     = var.web_port
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ 240926/var.tf
@@ -0,0 +1,4 @@
+variable "web_port" {
+  description = "the port the server will user for HTTP requests"
+  default     = 8080 # default 값 지정 가능
+}
